f264c83050a697624ea20946b67618597eb18176
Rishabh-Pandey007 <rishabhpandey2023@gmail.com>
2024-08-02 15:56:54 +0530

    code refactor of hero section

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,34 +1,31 @@
-import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/hero-img.png";
-import sun from "../../assets/sun.svg";
-import moon from "../../assets/moon.svg";
-import twitterLight from "../../assets/twitter-light.svg";
-import twitterDark from "../../assets/twitter-dark.svg";
-import githubLight from "../../assets/github-light.svg";
-import githubDark from "../../assets/github-dark.svg";
-import linkedInLight from "../../assets/linkedin-light.svg";
-import linkedInDark from "../../assets/linkedin-dark.svg";
-import CV from "../../assets/77RishabhPandey2025Resume.pdf";
-import { useTheme } from "../../common/ThemeContext";
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/Design uten navn.png';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/77RishabhPandey2025Resume.pdf';
+import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
-
   const { theme, toggleTheme } = useTheme();
 
-  const themeIcon = theme =='light' ? sun : moon;
-  const twitterIcon = theme =='light' ? twitterLight : twitterDark;
-  const githubIcon = theme =='light' ? githubLight : githubDark;
-  const linkedInIcon = theme =='light' ? linkedInLight : linkedInDark;
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
-
-
-   return (
-    <section id="hero">
+  return (
+    <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
         <img
-          className={styles.hero}
           src={heroImg}
-          alt="Profile picture of rishabh..."
+          className={styles.hero}
+          alt="Profile picture of Rishabh Pandey"
         />
         <img
           className={styles.colorMode}
@@ -37,40 +34,30 @@ function Hero() {
           onClick={toggleTheme}
         />
       </div>
-
       <div className={styles.info}>
         <h1>
           Rishabh
           <br />
           Pandey
         </h1>
-
-        <h2> Fullstack Developer</h2>
-
+        <h2>Full stack Developer</h2>
         <span>
-          <a href="twitter.com" target="_blank">
-            <img src={twitterIcon} alt="Twitter icon" />
+          {/* <a href="https://leetcode.com/u/RishabhPandey007/" target="_blank">
+            <img src={leetcodeIcon} alt="leetcode icon" />
+          </a> */}
+          <a href="https://github.com/Rishabh-Pandey007" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
           </a>
-
-          <a href="github.com" target="_blank">
-            <img src={githubIcon} alt="github icon" />
-          </a>
-
-          <a href="linkedIn.com" target="_blank">
-            <img src={linkedInIcon} alt="linkedIn icon" />
+          <a href="https://www.linkedin.com/in/rishabh-pandey-b97b7322a/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
         <p className={styles.description}>
           With a passion for developing modern React web apps for commercial
           businesses.
         </p>
-
-        {/* <a href={CV} download>
-          <button> Reeesume</button>
-        </a> */}
-
         <a href={CV} download>
-          <button className={styles.button}> Download CV</button>
+          <button className="hover">Download Resume</button>
         </a>
       </div>
     </section>
